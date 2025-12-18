a2bd56ad74605f526fccfd9103b97f522d78238a
Stefan Karger <stefan.eideloth@gmail.com>
2025-12-18 15:12:28 +0100

    oopsie

--- apps/docs/src/components/style-select.tsx
+++ apps/docs/src/components/style-select.tsx
@@ -6,28 +6,24 @@ export function StyleSelect() {
   const { style, setStyle } = useDesignSystem()
 
   return (
-    <>
-      <div>Style</div>
-      <Select
-        itemComponent={(props) => (
-          <SelectItem class="flex" item={props.item}>
-            {props.item.rawValue.icon}
-            {props.item.rawValue.title}
-          </SelectItem>
-        )}
-        onChange={(value) => {
-          if (value) setStyle(value)
-        }}
-        options={[...STYLES]}
-        optionTextValue="title"
-        optionValue="name"
-        value={style()}
-      >
-        <SelectTrigger aria-label="Style">
-          <SelectValue<Style>>{(state) => state.selectedOption().icon}</SelectValue>
-        </SelectTrigger>
-        <SelectContent />
-      </Select>
-    </>
+    <Select
+      itemComponent={(props) => (
+        <SelectItem class="flex" item={props.item}>
+          {props.item.rawValue.title}
+        </SelectItem>
+      )}
+      onChange={(value) => {
+        if (value) setStyle(value)
+      }}
+      options={[...STYLES]}
+      optionTextValue="title"
+      optionValue="name"
+      value={style()}
+    >
+      <SelectTrigger aria-label="Style">
+        <SelectValue<Style>>{(state) => state.selectedOption().title}</SelectValue>
+      </SelectTrigger>
+      <SelectContent />
+    </Select>
   )
 }
