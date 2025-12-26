1da22c36c5211244ed4b9e4bc2b6a45e5db17bc9
Kevin Abatan <abatan.k@gmail.com>
2025-12-26 15:54:03 +0100

    fix: format & better alignment with shadcn examples

--- apps/docs/src/registry/examples/avatar-demo.tsx
+++ apps/docs/src/registry/examples/avatar-demo.tsx
@@ -1,30 +1,30 @@
 import { Avatar, AvatarFallback, AvatarImage } from "~/registry/ui/avatar"
 
 export default function AvatarDemo() {
-	return (
-		<div class="flex flex-row flex-wrap items-center gap-12">
-			<Avatar>
-				<AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-				<AvatarFallback>CN</AvatarFallback>
-			</Avatar>
-			<Avatar class="rounded-lg">
-				<AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
-				<AvatarFallback>ER</AvatarFallback>
-			</Avatar>
-			<div class="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
-				<Avatar>
-					<AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-					<AvatarFallback>CN</AvatarFallback>
-				</Avatar>
-				<Avatar>
-					<AvatarImage src="https://github.com/maxleiter.png" alt="@maxleiter" />
-					<AvatarFallback>LR</AvatarFallback>
-				</Avatar>
-				<Avatar>
-					<AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
-					<AvatarFallback>ER</AvatarFallback>
-				</Avatar>
-			</div>
-		</div>
-	)
+  return (
+    <div class="flex flex-row flex-wrap items-center gap-12">
+      <Avatar>
+        <AvatarImage alt="@shadcn" src="https://github.com/stefan-karger.png" />
+        <AvatarFallback>EK</AvatarFallback>
+      </Avatar>
+      <Avatar class="rounded-lg">
+        <AvatarImage alt="@carere" src="https://github.com/carere.png" />
+        <AvatarFallback>CA</AvatarFallback>
+      </Avatar>
+      <div class="-space-x-2 flex *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:ring-background *:data-[slot=avatar]:grayscale">
+        <Avatar>
+          <AvatarImage alt="@shadcn" src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Avatar>
+          <AvatarImage alt="@maxleiter" src="https://github.com/maxleiter.png" />
+          <AvatarFallback>LR</AvatarFallback>
+        </Avatar>
+        <Avatar>
+          <AvatarImage alt="@evilrabbit" src="https://github.com/evilrabbit.png" />
+          <AvatarFallback>ER</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  )
 }
